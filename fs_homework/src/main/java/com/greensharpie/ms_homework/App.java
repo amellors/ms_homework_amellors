e20d2020895b9dab935c0af751c9fcade2acf798
Andrew Mellors <amellors@gmail.com>
2023-10-23 13:32:20 +0000

    Initial Setup

---
+++ fs_homework/src/main/java/com/greensharpie/ms_homework/App.java
@@ -0,0 +1,13 @@
+package com.greensharpie.ms_homework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
