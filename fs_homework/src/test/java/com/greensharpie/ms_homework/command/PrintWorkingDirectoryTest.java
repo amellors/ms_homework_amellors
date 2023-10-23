7e702b909298c8670f5debf9b7336a20fd0fda6b
Andrew Mellors <amellors@gmail.com>
2023-10-23 21:38:52 +0000

    Adds PrintWorking Directory command

---
+++ fs_homework/src/test/java/com/greensharpie/ms_homework/command/PrintWorkingDirectoryTest.java
@@ -0,0 +1,76 @@
+package com.greensharpie.ms_homework.command;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.greensharpie.ms_homework.filesystem.Directory;
+import com.greensharpie.ms_homework.system.SystemData;
+
+public class PrintWorkingDirectoryTest {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    public void rootDir()
+    {
+        SystemData system_data = new SystemData();
+
+        new PrintWorkingDirectory().exec(system_data);
+
+        assertEquals("/", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    public void rootWithMultipleDirs()
+    {
+        SystemData system_data = new SystemData();
+        String[] dirNames = {"newDir1", "newDir2", "newDir3", "newDir4"};
+        for (String name: dirNames) {
+            new MakeDirectory(name).exec(system_data);
+        }
+        new PrintWorkingDirectory().exec(system_data);
+
+        assertEquals("/", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    public void oneDirPath()
+    {
+        SystemData system_data = new SystemData();
+        String[] dirNames = {"newDir1", "newDir2", "newDir3", "newDir4"};
+        for (String name: dirNames) {
+            new MakeDirectory(name).exec(system_data);
+        }
+        new ChangeDirectory("newDir3").exec(system_data);
+        new PrintWorkingDirectory().exec(system_data);
+
+        assertEquals("/newDir3", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    public void multipleDirPath()
+    {
+        SystemData system_data = new SystemData();
+        String[] dirNames = {"newDir1", "newDir2", "newDir3", "newDir4"};
+        for (String name: dirNames) {
+            new MakeDirectory(name).exec(system_data);
+        }
+        new ChangeDirectory("newDir3").exec(system_data);
+        for (String name: dirNames) {
+            new MakeDirectory(name).exec(system_data);
+        }
+        new ChangeDirectory("newDir2").exec(system_data);
+        new PrintWorkingDirectory().exec(system_data);
+
+        assertEquals("/newDir3/newDir2", outputStreamCaptor.toString().trim());
+    }  
+}
