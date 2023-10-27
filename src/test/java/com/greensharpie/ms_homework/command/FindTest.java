18e2bbb6ecb636666a3473d3629148d0eadba569
Andrew Mellors <amellors@gmail.com>
2023-10-27 02:02:54 +0000

    Adding recursive find

--- src/test/java/com/greensharpie/ms_homework/command/FindTest.java
+++ src/test/java/com/greensharpie/ms_homework/command/FindTest.java
@@ -8,6 +8,7 @@ import java.io.PrintStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.greensharpie.ms_homework.filesystem.Directory;
 import com.greensharpie.ms_homework.system.SystemData;
 
 public class FindTest {
@@ -25,7 +26,7 @@ public class FindTest {
 
         new Find("No_File").exec(system_data);
 
-        assertEquals("No entry with name No_File was found.", outputStreamCaptor.toString().trim());
+        assertEquals("", outputStreamCaptor.toString().trim());
     }
 
     @Test
@@ -49,4 +50,62 @@ public class FindTest {
 
         assertEquals("Found a File with name newFile", outputStreamCaptor.toString().trim());
     }
+
+    /// =============== Recursive finds:
+
+    @Test
+    public void emptyDir_recurse()
+    {
+        SystemData system_data = new SystemData();
+
+        new Find("No_File", true).exec(system_data);
+
+        assertEquals("", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    public void directoryItem_recurse()
+    {
+        SystemData system_data = new SystemData();
+
+        new MakeDirectory("newDir").exec(system_data);
+        new Find("newDir", true).exec(system_data);
+
+        assertEquals("Found a Directory with name newDir", outputStreamCaptor.toString().trim());
+    }
+    
+    @Test
+    public void fileItem_recurse()
+    {
+        SystemData system_data = new SystemData();
+
+        new TouchFile("newFile").exec(system_data);
+        new Find("newFile", true).exec(system_data);
+
+        assertEquals("Found a File with name newFile", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    public void deepFind_recurse()
+    {
+        SystemData system_data = new SystemData();
+        Directory root = system_data.getCwd();
+
+        new MakeDirectory("newDir").exec(system_data);
+        new ChangeDirectory("newDir").exec(system_data);
+        new MakeDirectory("newDir2").exec(system_data);
+        new ChangeDirectory("newDir2").exec(system_data);
+        new TouchFile("newFile").exec(system_data);
+        new MakeDirectory("newDir3").exec(system_data);
+        new ChangeDirectory("newDir3").exec(system_data);
+        new TouchFile("newFile").exec(system_data);
+        system_data.setCwd(root);
+
+
+        new Find("newFile", true).exec(system_data);
+
+        assertEquals("Found a File with name newDir/newDir2/newFile\nFound a File with name newDir/newDir2/newDir3/newFile", outputStreamCaptor.toString().trim());
+    }
+
+
 }
